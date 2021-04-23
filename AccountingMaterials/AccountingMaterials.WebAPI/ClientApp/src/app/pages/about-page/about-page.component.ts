08dc958671d0209d176291d286365de83a0df4b8
Kiryl Shamiakou <Kiryl_Shamiakou@epam.com>
2021-04-23 18:13:52 +0300

    Added new front

---
+++ AccountingMaterials/AccountingMaterials.WebAPI/ClientApp/src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kks-about-page',
+  templateUrl: './about-page.component.html',
+  styleUrls: ['./about-page.component.scss']
+})
+export class AboutPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
