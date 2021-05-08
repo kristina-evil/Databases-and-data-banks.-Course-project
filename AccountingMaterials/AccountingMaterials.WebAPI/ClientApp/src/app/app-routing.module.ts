37e6f20e75479108d681aeb876e34a4616ef69fa
worm <62815248+alex-worm@users.noreply.github.com>
2021-05-08 11:12:48 +0300

    added material details page, improved experience from other pages

--- AccountingMaterials/AccountingMaterials.WebAPI/ClientApp/src/app/app-routing.module.ts
+++ AccountingMaterials/AccountingMaterials.WebAPI/ClientApp/src/app/app-routing.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { DetailsPageComponent } from './pages/details-page/details-page.component';
-import { MaterialsPageComponent } from './pages/materials-page/materials-page.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutPageComponent} from './pages/about-page/about-page.component';
+import {EmployeesPageComponent} from './pages/employees-page/employees-page.component';
+import {EmployeeDetailsPageComponent} from './pages/employee-details-page/employee-details-page.component';
+import {MaterialsPageComponent} from './pages/materials-page/materials-page.component';
+import {MaterialDetailsComponent} from './pages/material-details-page/material-details.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: 'main', component: HomePageComponent },
-  { path: 'materials', component: MaterialsPageComponent },
-  { path: 'about', component: AboutPageComponent },
-  { path: 'contacts', redirectTo: 'about' },
-  { path: 'details/:id', component: DetailsPageComponent },
+  {path: '', redirectTo: 'employees', pathMatch: 'full'},
+  {path: 'employees', component: EmployeesPageComponent},
+  {path: 'materials', component: MaterialsPageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'contacts', redirectTo: 'about'},
+  {path: 'employee-details/:id', component: EmployeeDetailsPageComponent},
+  {path: 'material-details/:id', component: MaterialDetailsComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- AccountingMaterials/AccountingMaterials.WebAPI/ClientApp/src/app/app.module.ts
+++ AccountingMaterials/AccountingMaterials.WebAPI/ClientApp/src/app/app.module.ts
@@ -1,29 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from 'src/material.module';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { DetailsPageComponent } from './pages/details-page/details-page.component';
-import { FormsModule } from '@angular/forms';
-import { EmployeesDataService } from './services/employees.service';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialsPageComponent } from './pages/materials-page/materials-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EmployeesPageComponent} from './pages/employees-page/employees-page.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import {NavbarComponent} from './layout/navbar/navbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from 'src/material.module';
+import {AboutPageComponent} from './pages/about-page/about-page.component';
+import {EmployeeDetailsPageComponent} from './pages/employee-details-page/employee-details-page.component';
+import {FormsModule} from '@angular/forms';
+import {EmployeesDataService} from './services/employees.service';
+import {HttpClientModule} from '@angular/common/http';
+import {MaterialsPageComponent} from './pages/materials-page/materials-page.component';
+import {MaterialDetailsComponent} from './pages/material-details-page/material-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
+    EmployeesPageComponent,
     FooterComponent,
     NavbarComponent,
     AboutPageComponent,
-    DetailsPageComponent,
+    EmployeeDetailsPageComponent,
     MaterialsPageComponent,
+    MaterialDetailsComponent,
   ],
   imports: [
     FormsModule,
@@ -36,4 +38,5 @@ import { MaterialsPageComponent } from './pages/materials-page/materials-page.co
   providers: [EmployeesDataService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
